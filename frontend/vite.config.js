81679ef3a754a4aada6f69d4bd3af7f3f35e8098
Anshu <anshudhaker3238@gmail.com>
2026-05-23 11:59:20 +0530

    Fix Railway allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,11 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 8080,
+    allowedHosts: ['romantic-nature-production-ecf4.up.railway.app']
+  }
+})
